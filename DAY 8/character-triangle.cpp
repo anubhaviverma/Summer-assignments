9966d1798c768216355169ad8d98795d35b385f1
anubhaviverma <anubhaviverma284@gmail.com>
2026-06-08 15:10:32 +0530

    Add files via upload

---
+++ DAY 8/character-triangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int j,i;
+    char name;
+    for(int i = 1;i<=5;i++){
+        char name='A';
+        for( int j=1;j<=i+1;j++){
+            cout<<name<<" ";
+                 name++;}
+        
+     cout<<endl;}
+    return 0;
+}
